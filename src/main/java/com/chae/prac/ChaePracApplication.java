22e27e6883563143c585328f2da0edf12db239aa
chaeee96 <1ouorrr@gmail.com>
2021-06-27 15:35:35 +0900

    첫 커밋

---
+++ src/main/java/com/chae/prac/ChaePracApplication.java
@@ -0,0 +1,13 @@
+package com.chae.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaePracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaePracApplication.class, args);
+	}
+
+}
